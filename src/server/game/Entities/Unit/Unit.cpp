3eb68949bb0d505e94219c44d4f935e7bfe6179c
Staleness89 <Staleness665@live.com>
2017-05-14 18:22:35 -0700

    part 1

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13195,7 +13195,7 @@ uint32 Unit::GetModelForForm(ShapeshiftForm form, uint32 spellId) const
         switch (form)
         {
 		case FORM_CAT:
-			if (HasAura(54912))    // Glyph of the Red lynx
+
 				return 15593;
 			else if (HasAura(58133))   // Glyph of the yellow lynx?
 				return 18167;
