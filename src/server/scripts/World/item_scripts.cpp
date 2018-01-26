1a61073441d79ac57b4bf9c162f5d9838f0d5781
Staleness89 <Staleness665@live.com>
2018-01-25 18:55:37 -0800

    Revert "shouldn't need this"
    This reverts commit cc8c6170ab047ec1bba9155b2e0986c3ec84302f.

--- src/server/scripts/World/item_scripts.cpp
+++ src/server/scripts/World/item_scripts.cpp
@@ -437,6 +437,9 @@ public:
 		if (!player->HasSkill(SKILL_LANG_COMMON))
 		{
 			player->LearnDefaultSkill(SKILL_LANG_COMMON, 300);
+			player->CastSpell(player, SPELL_LEARN_COMMON, true, item);
+			
+			player->DestroyItemCount(ITEM_COMMON_LANGUAGE, 1, true);
 			return true;
 		}
 		else
@@ -457,6 +460,9 @@ public:
 		if (!player->HasSkill(SKILL_LANG_ORCISH))
 		{
 			player->LearnDefaultSkill(SKILL_LANG_ORCISH, 300);
+			player->CastSpell(player, SPELL_LEARN_ORCISH, true, item);
+
+			player->DestroyItemCount(ITEM_ORCISH_LANGUAGE, 1, true);
 			return true;
 		}
 		else
